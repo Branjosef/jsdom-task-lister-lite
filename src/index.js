325de6a68315cad346a505cebc2c10a0a0085247
Brandon Fonnemann <branjosef@gmail.com>
2020-10-28 11:56:48 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  todo()
 });
+
+
+function todo () {
+
+  const button = document.getElementById('create-task-form')
+  button.addEventListener('submit', function(event){
+    event.preventDefault();
+  const inputBox = event.target.querySelector('#new-task-description');
+  const list = document.getElementById('tasks');
+  const input = document.createElement('li');
+  const deletebtn = document.createElement('button');
+  deletebtn.innerText = "X"
+  deletebtn.addEventListener('click', function(){
+    input.remove();
+  });
+  input.textContent = inputBox.value;
+   list.appendChild(input);
+   list.appendChild(deletebtn);
+  });
+}
+
