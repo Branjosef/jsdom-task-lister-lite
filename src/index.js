13d37d8694c37e5058c6c1d36053d1b892253cbe
Brandon Fonnemann <branjosef@gmail.com>
2020-10-28 12:02:43 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -2,23 +2,39 @@ document.addEventListener("DOMContentLoaded", () => {
   todo()
 });
 
-
 function todo () {
-
   const button = document.getElementById('create-task-form')
-  button.addEventListener('submit', function(event){
-    event.preventDefault();
+  button.addEventListener('submit', function(event){event.preventDefault();
   const inputBox = event.target.querySelector('#new-task-description');
   const list = document.getElementById('tasks');
   const input = document.createElement('li');
   const deletebtn = document.createElement('button');
   deletebtn.innerText = "X"
-  deletebtn.addEventListener('click', function(){
-    input.remove();
-  });
+  deletebtn.addEventListener('click', function(){input.remove()});
   input.textContent = inputBox.value;
    list.appendChild(input);
-   list.appendChild(deletebtn);
+   input.appendChild(deletebtn);
   });
 }
+
+
+// document.addEventListener("DOMContentLoaded", () => {
+ 
+//   document.getElementById("create-task-form").addEventListener("submit", function(event) {
+//     event.preventDefault();
+//     const ans = document.getElementById("new-task-description").value
+//     const li = document.createElement("li")
+//     const button = document.createElement("button")
+//     li.innerText = ans + " "
+//     button.setAttribute("data-desrciption", ans)
+//     button.innerHTML = "X"
+//     button.addEventListener('click', deleteTask)
+//     li.appendChild(button)
+//     document.getElementById("tasks").appendChild(li) 
+//     function deleteTask() {
+//       li.remove()
+//     }
+//   }, 
+//   false);
+// });
 
